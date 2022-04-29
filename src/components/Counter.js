5f18b7933db10b6ad7aefc556363db0a23e2af7a
moha5145 <mms.jimma@gmail.com>
2022-04-29 22:17:55 +0200

    reset btn

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,33 +4,39 @@ const Counter = ({tab, setTab}) => {
     
             return (<div key={index}>
                 <div className='counter'>
-                    <button 
+                    {counter > 0 && 
+                        <button 
                         onClick={()=> {
                             
                             let newTab = [...tab]
                             newTab[index] = newTab[index] -1 
-                            console.log(newTab)
+                            console.log(counter)
                             setTab(newTab)
                             // console.log(newTab)
                             
                         }}
                     >-</button>
+                    }
     
                     <div className='display bg-purple color-white'>
                         {counter}
                     </div>
 
-                    <button onClick={()=> {
+                    {counter < 10 && <button onClick={()=> {
                         
-                            let newTab = [...tab]
-                            newTab[index] = newTab[index] +1 
-                            setTab(newTab)
-                       
-                    }}>+</button>
+                        let newTab = [...tab]
+                        newTab[index] = newTab[index] +1 
+                        setTab(newTab)
+                   
+                }}>+</button>
+
+                    }
                     </div>
     
                     <button className='reset bg-purple color-white' onClick={()=> {
-                         setTab([0])
+                        let newTab = [...tab]
+                        newTab[index] = newTab[index] = 0
+                         setTab(newTab)
                     }}
     
                     >reset</button>
